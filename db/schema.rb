107e95b5356900c2f0107d7ad37623ad9f1d2c2e
ZumeShougo <g0109725rena@gmail.com>
2021-09-29 12:56:30 +0000

    [update]4-10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_121516) do
+ActiveRecord::Schema.define(version: 2021_09_29_113257) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "opinion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
