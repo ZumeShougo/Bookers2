3b491640a0a1436b1610f42998e681a573271c06
ZumeShougo <g0109725rena@gmail.com>
2021-10-19 12:15:38 +0000

    [update]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
+
+  post "users" => "books#create"
+
+  post "users/:id" => "books#create"
+
+  post "books" => "books#create"
 end
