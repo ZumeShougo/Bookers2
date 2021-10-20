58ea8efe66a1fbfa50a26bc275e20dddb7b704bf
ZumeShougo <g0109725rena@gmail.com>
2021-10-20 12:51:09 +0000

    [update]4-10]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2d38223e3ee003ed8afae0db47a1a8327fdedb07fb61391cb96985942bac83f66303bedcf2f07c8cf010563a342b2b53455f42170cb4925d99a76658fb86c6af'
